3b4f6fa562ad2220a45278f640dda44518bf663b
Bogdan <bogdan.fad@yandex.ru>
2022-12-18 00:27:46 +0300

    number 2

---
+++ number2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. ");
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+   string str = Convert.ToString(a);
+   Console.Write("Третья цифра: ");
+   Console.WriteLine(a/100%10); 
+}
