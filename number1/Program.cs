195af8c39f10e4b8c3b318a3e056d7d810d5113a
Bogdan <bogdan.fad@yandex.ru>
2022-12-17 23:09:07 +0300

    Task 10 complete

---
+++ number1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. ");
+Console.WriteLine("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a > 999 || a < 100)
+{
+    Console.WriteLine("Число введено неверно!");
+}
+else
+{
+   Console.Write("Второй цифрой является: ");
+   Console.WriteLine(a/10%10); 
+}
